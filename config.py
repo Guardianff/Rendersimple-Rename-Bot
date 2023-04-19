e1ed623dcda831ef3fa60f3c18991a4544a38ecd
Guardianff <103484627+Guardianff@users.noreply.github.com>
2023-04-19 13:54:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-ADMIN = int(environ.get("ADMIN", ""))          
+API_ID = int(environ.get("API_ID", "15257211"))
+API_HASH = environ.get("API_HASH", "8b7cf73ce577720d74a213bbb98f4104")
+BOT_TOKEN = environ.get("BOT_TOKEN", "6101280002:AAEtrIYyC7JQwxGhPZDDyNQgYZ02lqTsdtY")
+ADMIN = int(environ.get("ADMIN", "1350212613"))          
 CAPTION = environ.get("CAPTION", "")
 
 class temp(object):
-    THUMBNAIL = environ.get("THUMBNAIL", "")
+    THUMBNAIL = environ.get("THUMBNAIL", "https://te.legra.ph/file/f7bb87698b34dd8ba46c1.jpg")
