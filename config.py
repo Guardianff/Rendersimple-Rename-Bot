ccab9a154bcf585315b7aa321a5158d6702a742d
Guardianff <103484627+Guardianff@users.noreply.github.com>
2023-07-24 22:24:54 +0530

    config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ API_ID = int(environ.get("API_ID", "15257211"))
 API_HASH = environ.get("API_HASH", "8b7cf73ce577720d74a213bbb98f4104")
 BOT_TOKEN = environ.get("BOT_TOKEN", "6067195140:AAGUO0M-2z34L4061BLbY9pjlDlCHJ4in9c")
 ADMIN = int(environ.get("ADMIN", "1350212613"))          
-CAPTION = environ.get("CAPTION", "<b>{new_name}</b>")
+CAPTION = environ.get("CAPTION", "<b>{file_name}</b>")
 
 class temp(object):
     THUMBNAIL = environ.get("THUMBNAIL", "AgACAgUAAxkBAAILDmSfIWDUFX4YwnF1WHIU0iDNg7uMAAIfuzEbzs34VDxhYfdEEc4OAAgBAAMCAAN4AAceBA")
